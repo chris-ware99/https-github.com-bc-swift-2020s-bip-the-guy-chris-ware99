a757b65df359e1c735fa5965a6a8c31e20da8c9d
Christopher Ware <warech@bc.edu>
2020-02-03 12:37:21 -0500

    Initial Commit

---
+++ Bip The Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip The Guy
+//
+//  Created by Chris Ware on 2/3/20.
+//  Copyright © 2020 Chris Ware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
